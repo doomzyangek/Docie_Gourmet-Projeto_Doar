008b67432434053d4da8372e6fa18bac7954ef0d
Rafael-Ribeiro-G <r.ribeirogoncalves07@gmail.com>
2025-11-25 19:00:10 -0300

    Dociê-Gourmet  0.5

---
+++ src/Pages/Doces.jsx
@@ -0,0 +1,10 @@
+function Doces() {
+  return (
+    <div>
+      <h1>Doces Page</h1>
+      <p>Welcome to the Doces page!</p>
+    </div>
+  );
+}
+
+export default Doces;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GlobalStyle from './GlobalStyle.js';
+import { createRoot } from 'react-dom/client';
 
 // Veja como a importação ficou mais curta:
 import { Home } from './components/Home';
 // O React automaticamente procura por './components/Header/index.jsx'
 import { Produtos } from './components/Produtos/produtos.jsx';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
