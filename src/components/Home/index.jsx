047393d5c4c77d320de4ec640dafcaa5ffd2cd57
Rafael-Ribeiro-G <r.ribeirogoncalves07@gmail.com>
2025-11-26 15:10:24 -0300

    versão Dociê-Gourmet 0.6.2.4

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-import fundo from '/src/assets/parede.png';
-=======
->>>>>>> 3df416de80d430b683c7b750248be3c0ebd35d49
 import { Homecontainer } from './styles.js';
 import Logo from '/src/assets/DocieLogo.png';
 
 export function Home() {
   return (
     <Homecontainer>
-      
+
 
       <div className="Logo">
         <img src={Logo} alt="Logo" />
